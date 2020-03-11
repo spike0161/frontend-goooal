2d64f0108d8a084c3d642375fb33fe3493381c1d
github username <github email address>
2020-03-11 14:20:12 -0400

    Turned the logos into links that link to their respective show page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Route, Switch} from 'react-router-dom'
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import TeamShow from "./components/TeamShow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      allTeams: []
+    }
+  }
+
+componentDidMount(){
+  fetch('http://localhost:3000/teams')
+  .then(res => res.json())
+  .then(teamsArr => this.setState({ allTeams: teamsArr}))
+}
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <HomePage allTeams={this.state.allTeams}/>
+        <TeamShow />
+      </div>
+    );
+  }
 }
 
 export default App;
