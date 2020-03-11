d9fad9c97653fa2dcbda2e755f3a18685cc79335
github username <github email address>
2020-03-11 15:31:05 -0400

    Created Basic rendered ShowPage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, BrowserRouter as Router } from 'react-router-dom'
+import NavBar from "./components/NavBar";
+import HomePage from "./components/HomePage";
+import TeamShow from "./components/TeamShow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      allTeams: []
+    }
+  }
+
+componentDidMount(){
+  fetch('http://localhost:3000/teams')
+  .then(res => res.json())
+  .then(teamsArr => this.setState({ allTeams: teamsArr}))
+}
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <NavBar />
+
+          <Route exact
+                 path='/'
+                 render={props => (
+                     <HomePage allTeams={this.state.allTeams}/>
+                 )}
+                 />
+            <Route path ='/teams/:id' render={(props) => {
+                    let id = parseInt(props.match.params.id)
+                    let teamObj = this.state.allTeams.find(team => team.id === id)
+                    return <TeamShow team={teamObj} />
+                    }
+                  }/>
+        </Router>
+
+      </div>
+    );
+  }
 }
 
 export default App;
