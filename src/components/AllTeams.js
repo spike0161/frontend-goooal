e01d5b47b72fbf9c6900999439fee391a1b41ba9
github username <github email address>
2020-03-19 13:23:34 -0400

    created search bar to filter all teams by name

--- src/components/AllTeams.js
+++ src/components/AllTeams.js
@@ -1,25 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const AllTeams = props => {
-  console.log(props);
-  return (
-    <div>
-      <h2>All Teams</h2>
-      {props.teams.map(team => (
-        <Link to={`/teams/${team.id}`}>
-          <img
-            className="team-img"
-            key={team.id}
-            style={{ width: 100, height: 100, margin: 20 }}
-            alt={team.shortName}
-            src={team.crestUrl}
+class AllTeams extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      searchText: ""
+    }
+  }
+
+searchTextHandler = (e) => {
+  e.preventDefault()
+  this.setState({ searchText: e.target.value})
+}
+
+getFilteredTeams = () => {
+  let filteredTeams = this.props.allTeams.filter( team => team.full_name.includes(this.state.searchText))
+}
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h2>All Teams</h2>
+        <div>
+          <input
+            className="search-teams"
+            type="text"
+            placeholder="Search Teams"
+            onChange ={ e => this.searchTextHandler(e)}
           />
-        </Link>
-      ))}
-    </div>
-  );
-};
+
+        </div>
+        {this.props.teams.map(team => (
+          <Link to={`/teams/${team.id}`}>
+            <div>
+              <img
+                className="team-img"
+                key={team.id}
+                style={{ width: 100, height: 100, margin: 20 }}
+                alt={team.shortName}
+                src={team.crestUrl}
+              />
+            <h3>{team.full_name}</h3>
+            </div>
+          </Link>
+
+        ))}
+      </div>
+    );
+  }
+}
 
 export default AllTeams;
 
