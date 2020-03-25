787fa19c592dcf3fd37a3694dfef51b9ea7364e2
github username <github email address>
2020-03-25 13:30:38 -0400

    Created dropdown menu for player  roster

--- src/components/TeamShow.js
+++ src/components/TeamShow.js
@@ -22,7 +22,9 @@ class TeamShow extends React.Component {
   //     .then(data => this.setState({ upComing: data.matches }));
   // }
   getPlayers = () => {
-    let players = this.props.players.filter(player => player.team_id == this.props.team.id)
+    let players = this.props.players.filter(
+      player => player.team_id == this.props.team.id
+    );
     return players;
   };
 
@@ -30,41 +32,44 @@ class TeamShow extends React.Component {
     console.log("TeamShow", this.getPlayers());
     return (
       <div>
-      <div>
-        {this.props.team ? (
-          <div>
-            <img
-              src={this.props.team.crestUrl}
-              style={{ width: 200, height: 200 }}
-              alt={this.props.team.shortName}
-            />
-            <h1>{this.props.team.full_name}</h1>
-            <h3>{this.props.team.short_name}</h3>
-            <h3>{this.props.team.tla}</h3>
-            <button
-              onClick={() => this.props.handleFavoriteTeam(this.props.team)}
-            >
-              Favorite
-            </button>
+        <div>
+          {this.props.team ? (
+            <div>
+              <img
+                src={this.props.team.crestUrl}
+                style={{ width: 200, height: 200 }}
+                alt={this.props.team.shortName}
+              />
+              <h1>{this.props.team.full_name}</h1>
+              <h3>{this.props.team.short_name}</h3>
+              <h3>{this.props.team.tla}</h3>
+              <button
+                onClick={() => this.props.handleFavoriteTeam(this.props.team)}
+              >
+                Favorite
+              </button>
 
-            <p>{this.props.team.address}</p>
-            <p>{this.props.team.phone}</p>
-            <p>{this.props.team.website}</p>
-            <p>{this.props.team.founded}</p>
-            <p>{this.props.team.email}</p>
-            <p>{this.props.team.club_colors}</p>
-            <p>{this.props.team.venue}</p>
-          </div>
-        ) : null}
-      </div>
-
-
-
-
+              <p>{this.props.team.address}</p>
+              <p>{this.props.team.phone}</p>
+              <p>{this.props.team.website}</p>
+              <p>{this.props.team.founded}</p>
+              <p>{this.props.team.email}</p>
+              <p>{this.props.team.club_colors}</p>
+              <p>{this.props.team.venue}</p>
+            </div>
+          ) : null}
+        </div>
 
         <div>
-
-          <table className='roster'>
+          <select className="ui search dropdown" onChange={e => console.log(e)}>
+            <option value="">All Players</option>
+            <option value="Goalkeeper">Goalkeeper</option>
+            <option value="Defender">Defender</option>
+            <option value="Midfielder">Midfielder</option>
+            <option value="Attacker">Attacker</option>
+            <option value="Coach">Coach</option>
+          </select>
+          <table className="roster">
             <tr>
               <th>Name</th>
               <th>Position</th>
@@ -72,17 +77,17 @@ class TeamShow extends React.Component {
               <th>Shirt Number</th>
             </tr>
             {this.getPlayers().map(player => (
-            <tr>
-              <td>{player.name}</td>
-              <td>{player.position}</td>
-              <td>{player.nationality}</td>
-              <td>{player.shirt_number}</td>
-            </tr>
-          ))}
+              <tr>
+                <td>{player.name}</td>
+                <td>{player.position}</td>
+                <td>{player.nationality}</td>
+                <td>{player.shirt_number}</td>
+              </tr>
+            ))}
           </table>
         </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
 
