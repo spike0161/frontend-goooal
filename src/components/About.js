586745d5e173f9ea7aa583caf8ba760870faf4fe
github username <github email address>
2020-03-26 19:47:44 -0400

    Player roster dropdown styled

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = (props) => {
     <div className="about-container">
     <img src="https://media-exp1.licdn.com/dms/image/C5603AQHfHgEetxP9Yg/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=Ch-wb7UspDO1_tsymanGev-HSDj2icfak3iQVCYCQ2o" alt="Profile Picture" id="profile-pic"/>
     <h3>Dean Hildebrand</h3>
-    <p>Fullstack Software Engineer</p>
+    <p><strong>Fullstack Software Engineer</strong></p>
 
     <a href="https://github.com/spike0161">
     <i className="fas fa fa-github"></i></a>
