eff5ff3bc7d16bdc2429f301fa3fb04be38413dd
github username <github email address>
2020-03-26 16:00:45 -0400

    Increased size of favorite team on user profile page

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -7,11 +7,11 @@ const TeamCard = props => {
       <img
         src={props.team.crestUrl}
         alt={props.team.shortName}
-        style={{ width: 50, height: 50 }}
+        style={{ width: 100, height: 100 }}
       />
       <h2>{props.team.tla}</h2>
 
-    <button onClick={() => props.removeFavoriteTeam(props.team.id)}> Remove </button>
+    <button className="btn btn-light btn-lg" onClick={() => props.removeFavoriteTeam(props.team.id)}> Remove </button>
 
     </div>
   );
