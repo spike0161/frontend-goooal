ca9524cf20464e526546618f4664fe2a3e0a5b3a
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-04-02 14:55:16 -0400

    Styled input text box on login form

--- src/containers/login.js
+++ src/containers/login.js
@@ -23,14 +23,43 @@ export default class Login extends Component {
       return <Redirect to="/profile" />
     }
     return (
-      <div>
-        <form onSubmit={this.submitHandler}>
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
-          <input type="submit" value="Login" />
 
-        </form>
+      <div id="login-form-container">
+      <form>
+          <div className="form-group row">
+              <label for="inputUsername" className="col-sm-2 col-form-label label" >Username</label>
+              <div className="col-sm-10">
+                  <input type="username" className="form-control" id="inputUsername" placeholder="Username..." />
+              </div>
+          </div>
+          <div className="form-group row">
+              <label for="inputPassword" className="col-sm-2 col-form-label label">Password</label>
+              <div className="col-sm-10">
+                  <input type="password" className="form-control" id="inputPassword" placeholder="Password..." />
+              </div>
+          </div>
+          <div className="form-group row">
+              <div className="col-sm-10 offset-sm-2">
+                  <button type="submit" className="btn btn-primary">Sign in</button>
+              </div>
+          </div>
+      </form>
       </div>
+
+
     )
   }
 }
+
+//
+// <form onSubmit={this.submitHandler}>
+// <div className="form-group row">
+// <label for="inputUsername" className="col-sm-2 col-form-label">Username</label>
+// <div className="col-sm-10">
+// <input type="text" className="form-control" placeholder="username" />
+// </div>
+// <input type="password" placeholder="password" />
+// <input type="submit" value="Login" />
+//
+// </form>
+// </div>
